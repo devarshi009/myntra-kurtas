b032be63ea8f2d49c3e717f724c69085cb99f2eb
devarshi009 <devarshiojha009@gmail.com>
2023-01-22 20:46:27 +0530

    all

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express=require("express");
-const app=express();
-app.get("/",(req,res)=>{
-    res.send("Hello")
-})
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
 
-app.listen(process.env.PORT||4000,()=>{
-    console.log("app is running")
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
 })
